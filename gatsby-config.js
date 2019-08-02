febeacf1eab9317ea838a758772b6da6873978de
Dustin Schau <dustinschau@gmail.com>
2019-08-01 17:24:26 -0700

    chore: tweaks

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,12 +12,12 @@ module.exports = {
     {
       resolve: `gatsby-source-cosmicjs`,
       options: {
-        bucketSlug: process.env.COSMIC_BUCKET || `node-starter`,
+        bucketSlug: process.env.COSMIC_BUCKET || `9c2ade90-b4bb-11e9-87c5-ab736be982f7`,
         objectTypes: [`pages`],
         // If you have enabled read_key to fetch data (optional).
-        apiAccess: {
-          read_key: ``,
-        }
+        // apiAccess: {
+        //   read_key: `DDS1xPEhiFkcT5SXRzQ61pfC9oh470kelyB9SvUPL8KTRJwfia`,
+        // }
       }
     },
   ],
